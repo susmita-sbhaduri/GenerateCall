94b964e3d12366fe0ea3762c9e8499674cb432c0
sb <sb@sb>
2023-06-22 17:54:10 +0530

    first

--- src/main/java/org/bhaduri/generatecall/DataStoreNames.java
+++ src/main/java/org/bhaduri/generatecall/DataStoreNames.java
@@ -12,9 +12,9 @@ public class DataStoreNames {
 
     public static final String TICKER_DATA_DETAILS = "/home/sb/Documents/java_testing/EQ_test/";
 //    public static final String TICKER_DATA_SERIES = "/home/sb/Documents/java_testing/EQ_test_data/";
-    public static final String INPUT_CALL_DATA_PATH = "/home/sb/Documents/java_testing/calls19_matlab.csv";
-    public static final String OUTPUT_CALL_DATA_PATH = "/home/sb/Documents/java_testing/calls20java.csv";
-    public static final String INPUT_PRICE_DATA_PATH = "/home/sb/Documents/java_testing/price19_matlab.csv";
-    public static final String OUTPUT_PRICE_DATA_PATH = "/home/sb/Documents/java_testing/price20java.csv";
+    public static final String INPUT_CALL_DATA_PATH = "/home/sb/Documents/java_testing/calls21java.csv";
+    public static final String OUTPUT_CALL_DATA_PATH = "/home/sb/Documents/java_testing/calls22java.csv";
+    public static final String INPUT_PRICE_DATA_PATH = "/home/sb/Documents/java_testing/price21java.csv";
+    public static final String OUTPUT_PRICE_DATA_PATH = "/home/sb/Documents/java_testing/price22java.csv";
 
 }
